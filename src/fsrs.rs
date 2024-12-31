08ca960feb421246a00455a1c30f007e4fc311d5
Tim Harding <Tim@TimHarding.co>
2024-12-31 15:57:42 -0800

    refactor: Moved longterm test to longterm file

--- src/fsrs.rs
+++ src/fsrs.rs
@@ -137,49 +137,6 @@ mod tests {
     }
 
     #[test]
-    fn test_long_term_scheduler() {
-        let params = Parameters {
-            w: WEIGHTS,
-            enable_short_term: false,
-            ..Default::default()
-        };
-
-        let fsrs = Fsrs::new(params);
-        let mut card = Card::new();
-        let mut now = string_to_utc("2022-11-29 12:30:00 +0000 UTC");
-        let mut interval_history = vec![];
-        let mut stability_history = vec![];
-        let mut difficulty_history = vec![];
-
-        for rating in TEST_RATINGS.into_iter() {
-            let mut scheduler = fsrs.scheduler(card, now);
-            card = scheduler.next_card(rating);
-            let next = scheduler.next_card(rating);
-
-            assert_eq!(card, next);
-
-            interval_history.push(card.scheduled_days);
-            stability_history.push(card.stability.round_float(4));
-            difficulty_history.push(card.difficulty.round_float(4));
-            now = card.due;
-        }
-
-        let expected_interval = [3, 13, 48, 155, 445, 1158, 17, 3, 9, 27, 74, 190, 457];
-        let expected_stability = [
-            3.0412, 13.0913, 48.1585, 154.9373, 445.0556, 1158.0778, 16.6306, 2.9888, 9.4633,
-            26.9474, 73.9723, 189.7037, 457.4379,
-        ];
-        let expected_difficulty = [
-            4.4909, 4.2666, 4.0575, 3.8624, 3.6804, 3.5108, 5.219, 6.8122, 6.4314, 6.0763, 5.7452,
-            5.4363, 5.1483,
-        ];
-
-        assert_eq!(interval_history, expected_interval);
-        assert_eq!(stability_history, expected_stability);
-        assert_eq!(difficulty_history, expected_difficulty);
-    }
-
-    #[test]
     fn test_get_retrievability() {
         let fsrs = Fsrs::default();
         let card = Card::new();
--- src/scheduler/longterm.rs
+++ src/scheduler/longterm.rs
@@ -254,3 +254,100 @@ impl Longterm {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        models::{Card, Rating},
+        parameters::Parameters,
+        scheduler::longterm::Longterm,
+    };
+    use chrono::{DateTime, TimeZone, Utc};
+
+    static TEST_RATINGS: [Rating; 13] = [
+        Rating::Good,
+        Rating::Good,
+        Rating::Good,
+        Rating::Good,
+        Rating::Good,
+        Rating::Good,
+        Rating::Again,
+        Rating::Again,
+        Rating::Good,
+        Rating::Good,
+        Rating::Good,
+        Rating::Good,
+        Rating::Good,
+    ];
+
+    static WEIGHTS: [f64; 19] = [
+        0.4197, 1.1869, 3.0412, 15.2441, 7.1434, 0.6477, 1.0007, 0.0674, 1.6597, 0.1712, 1.1178,
+        2.0225, 0.0904, 0.3025, 2.1214, 0.2498, 2.9466, 0.4891, 0.6468,
+    ];
+
+    fn string_to_utc(date_string: &str) -> DateTime<Utc> {
+        let datetime = DateTime::parse_from_str(date_string, "%Y-%m-%d %H:%M:%S %z %Z").unwrap();
+        Utc.from_local_datetime(&datetime.naive_utc()).unwrap()
+    }
+
+    trait RoundFloat {
+        fn round_float(self, precision: i32) -> f64;
+    }
+
+    impl RoundFloat for f64 {
+        fn round_float(self, precision: i32) -> f64 {
+            let multiplier = 10.0_f64.powi(precision);
+            (self * multiplier).round() / multiplier
+        }
+    }
+
+    #[test]
+    fn test_long_term_scheduler() {
+        let params = Parameters {
+            w: WEIGHTS,
+            enable_short_term: false,
+            ..Default::default()
+        };
+
+        let mut card = Card::new();
+        let mut now = string_to_utc("2022-11-29 12:30:00 +0000 UTC");
+        let mut interval_history = vec![];
+        let mut stability_history = vec![];
+        let mut difficulty_history = vec![];
+
+        for rating in TEST_RATINGS.into_iter() {
+            let next = {
+                let mut scheduler = Longterm::new(params.clone(), card, now);
+                let schedule = scheduler.review(rating);
+                schedule.card
+            };
+
+            card = {
+                let mut scheduler = Longterm::new(params.clone(), card, now);
+                let schedule = scheduler.review(rating);
+                schedule.card
+            };
+
+            assert_eq!(card, next);
+
+            interval_history.push(card.scheduled_days);
+            stability_history.push(card.stability.round_float(4));
+            difficulty_history.push(card.difficulty.round_float(4));
+            now = card.due;
+        }
+
+        let expected_interval = [3, 13, 48, 155, 445, 1158, 17, 3, 9, 27, 74, 190, 457];
+        let expected_stability = [
+            3.0412, 13.0913, 48.1585, 154.9373, 445.0556, 1158.0778, 16.6306, 2.9888, 9.4633,
+            26.9474, 73.9723, 189.7037, 457.4379,
+        ];
+        let expected_difficulty = [
+            4.4909, 4.2666, 4.0575, 3.8624, 3.6804, 3.5108, 5.219, 6.8122, 6.4314, 6.0763, 5.7452,
+            5.4363, 5.1483,
+        ];
+
+        assert_eq!(interval_history, expected_interval);
+        assert_eq!(stability_history, expected_stability);
+        assert_eq!(difficulty_history, expected_difficulty);
+    }
+}
