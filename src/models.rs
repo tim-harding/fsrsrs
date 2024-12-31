eabf5b4fa0c8267a7e91b789dec8a1d1076633fd
Tim Harding <Tim@TimHarding.co>
2024-12-31 15:52:01 -0800

    refactor!: Delete Rating::iter_variants
    BREAKING CHANGE: This is no longer used since RecordLog was removed.

--- src/models.rs
+++ src/models.rs
@@ -45,12 +45,6 @@ pub enum Rating {
     Easy = 4,
 }
 
-impl Rating {
-    pub fn iter_variants() -> impl Iterator<Item = Self> {
-        [Rating::Again, Rating::Hard, Rating::Good, Rating::Easy].into_iter()
-    }
-}
-
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Review {
