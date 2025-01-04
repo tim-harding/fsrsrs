1c61821783a12f89eca10fb6ce56a2549ac0b52c
Tim Harding <Tim@TimHarding.co>
2025-01-03 21:33:01 -0800

    fix: Remove serde derive for Card

--- src/card.rs
+++ src/card.rs
@@ -2,7 +2,6 @@ use crate::{to_days, Duration, Grade, Time};
 
 /// The state of FSRS after a review
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Card {
     /// When the card was last reviewed
     pub when: Time,
